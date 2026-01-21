f1b6ee92d42cff934b53baacf2b48aec2d4940d4
torfs2003 <lasse.torfs@gmail.com>
2026-01-21 12:49:54 +0100

    Finalisatie

---
+++ build/CMakeFiles/SQLGuard.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SQLGuard.dir/LALR.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/LALR.cpp.obj.d"
+  "CMakeFiles/SQLGuard.dir/auth/RBACManager.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/auth/RBACManager.cpp.obj.d"
+  "CMakeFiles/SQLGuard.dir/auth/User.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/auth/User.cpp.obj.d"
+  "CMakeFiles/SQLGuard.dir/lexer/Lexer.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/lexer/Lexer.cpp.obj.d"
+  "CMakeFiles/SQLGuard.dir/main.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/main.cpp.obj.d"
+  "CMakeFiles/SQLGuard.dir/parser/LALRParser.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/parser/LALRParser.cpp.obj.d"
+  "CMakeFiles/SQLGuard.dir/security/SecurityAnalyzer.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/security/SecurityAnalyzer.cpp.obj.d"
+  "CMakeFiles/SQLGuard.dir/security/TaintAnalyzer.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/security/TaintAnalyzer.cpp.obj.d"
+  "CMakeFiles/SQLGuard.dir/utils/QueryRunner.cpp.obj"
+  "CMakeFiles/SQLGuard.dir/utils/QueryRunner.cpp.obj.d"
+  "SQLGuard.exe"
+  "SQLGuard.exe.manifest"
+  "SQLGuard.pdb"
+  "libSQLGuard.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SQLGuard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
